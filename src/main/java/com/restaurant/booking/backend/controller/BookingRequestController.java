4b099d2ca3468468b343f3135e056f2c2167fa06
9johan2 <67582307+9johan2@users.noreply.github.com>
2021-08-15 17:40:37 +0200

    Add files via upload

--- src/main/java/com/restaurant/booking/backend/controller/BookingRequestController.java
+++ src/main/java/com/restaurant/booking/backend/controller/BookingRequestController.java
@@ -3,14 +3,13 @@ package com.restaurant.booking.backend.controller;
 import com.restaurant.booking.backend.model.BookingRequest;
 import com.restaurant.booking.backend.service.BookingRequestService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("api/v1/activeRequests")
+@RequestMapping("api/bookings")
 public class BookingRequestController {
 
     private final BookingRequestService service;
@@ -21,8 +20,33 @@ public class BookingRequestController {
     }
 
     @GetMapping
-    public List<BookingRequest> getActiveRequests() {
-        return service.getActiveRequests();
+    public List<BookingRequest> getAllBookings() {
+        return service.getAllBookings();
     }
+
+    @GetMapping("/status")
+    public List<BookingRequest> getBooking(
+            @RequestParam(value = "status", required = false) BookingRequest.Status status) {
+
+        return service.getBookings(status);
+    }
+
+    @GetMapping("/id")
+    public Optional<BookingRequest> getBookingById(
+            @RequestParam(value = "id", required = false) String id) {
+
+        return service.getByID(id);
+    }
+
+    @PostMapping
+    public void save(BookingRequest bookingRequest) {
+        service.save(bookingRequest);
+    }
+
+    @PutMapping
+    public void update(BookingRequest bookingRequest) {
+        service.save(bookingRequest);
+    }
+
 
 }
--- src/main/java/com/restaurant/booking/backend/controller/TableController.java
+++ src/main/java/com/restaurant/booking/backend/controller/TableController.java
@@ -3,14 +3,13 @@ package com.restaurant.booking.backend.controller;
 import com.restaurant.booking.backend.model.Table;
 import com.restaurant.booking.backend.service.TableService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/tables")
+@RequestMapping("api/tables")
 public class TableController {
 
     private final TableService tableService;
@@ -20,8 +19,21 @@ public class TableController {
         this.tableService = tableService;
     }
 
-    @GetMapping
-    public List<Table> getTables() {
+    @GetMapping()
+    public List<Table> getTables(
+            @RequestParam(value = "requestedTime", required = false)LocalDateTime requestedTime,
+            @RequestParam(value = "numOfPeople", required = false) Integer numOfPeople
+            ) {
+        if (requestedTime != null && numOfPeople != null) {
+            return tableService.getAvailableTables(requestedTime, numOfPeople);
+        }
         return tableService.getTables();
+    }
+
+    @PutMapping
+    public void save(@RequestParam("table") Table table) {
+        if (table != null) {
+            tableService.save(table);
+        }
     }
 }
