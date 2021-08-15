c7d9242a7c5d13f873a9b3e1e15f74bd39339471
9johan2 <67582307+9johan2@users.noreply.github.com>
2021-08-15 17:44:55 +0200

    Update ActiveBookings.java

--- src/main/java/com/restaurant/booking/ui/bookings/active/ActiveBookings.java
+++ src/main/java/com/restaurant/booking/ui/bookings/active/ActiveBookings.java
@@ -1,7 +1,7 @@
 package com.restaurant.booking.ui.bookings.active;
 
+import com.restaurant.booking.backend.controller.BookingRequestController;
 import com.restaurant.booking.backend.model.BookingRequest;
-import com.restaurant.booking.backend.service.BookingRequestService;
 import com.restaurant.booking.ui.MainLayout;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -11,13 +11,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 @Route(value = "active-bookings", layout = MainLayout.class)
 public class ActiveBookings extends VerticalLayout {
 
-    private final BookingRequestService bookingRequestService;
+    private final BookingRequestController bookingRequestController;
 
-    private Grid<BookingRequest> grid = new Grid<>(BookingRequest.class);
+    private final Grid<BookingRequest> grid = new Grid<>(BookingRequest.class);
 
     @Autowired
-    public ActiveBookings(BookingRequestService bookingRequestService) {
-        this.bookingRequestService = bookingRequestService;
+    public ActiveBookings(BookingRequestController bookingRequestController) {
+        this.bookingRequestController = bookingRequestController;
 
         setSizeFull();
         configureGrid();
@@ -35,6 +35,7 @@ public class ActiveBookings extends VerticalLayout {
     }
 
     private void updateList() {
-        grid.setItems(bookingRequestService.getActiveRequests());
+        grid.setItems(bookingRequestController.getBooking(BookingRequest.Status.APPROVED));
     }
+
 }
