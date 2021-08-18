da11a08ac989245323b42e6a23ebf290eaf0937c
9johan2 <67582307+9johan2@users.noreply.github.com>
2021-08-18 12:55:16 +0200

    Change dialog appearence
    Change dialog to be more readable.
    Add a method for dialog configuration.
    Add a button to the dialog to not accidentally close it.
    Update background so it does not show error messages on successful booking.
    The page automatically refreshes when the dialog is closed.

--- src/main/java/com/restaurant/booking/ui/bookings/newbooking/NewBooking.java
+++ src/main/java/com/restaurant/booking/ui/bookings/newbooking/NewBooking.java
@@ -4,8 +4,11 @@ import com.restaurant.booking.backend.controller.BookingRequestController;
 import com.restaurant.booking.backend.controller.TableController;
 import com.restaurant.booking.ui.MainLayout;
 import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,9 +19,11 @@ public class NewBooking extends VerticalLayout {
 
     private final TableController tableController;
     private final BookingRequestController bookingRequestController;
-    private NewBookingForm form;
+    private final NewBookingForm form;
 
     private Dialog dialog;
+    private VerticalLayout dialogDisplay;
+    private Text bookingId;
 
     @Autowired
     public NewBooking(TableController tableController,
@@ -32,19 +37,41 @@ public class NewBooking extends VerticalLayout {
         this.form = new NewBookingForm(tableController);
         form.addListener(NewBookingForm.SaveEvent.class, this::saveBooking);
 
-        this.dialog = new Dialog();
-        dialog.setWidth("500px");
-        dialog.setHeight("200px");
-
+        configureDialog();
 
         add(header,form, dialog);
+    }
+
+    private void configureDialog() {
+        this.dialog = new Dialog();
+        this.dialogDisplay = new VerticalLayout();
+        this.bookingId = new Text("");
+
+        dialog.setWidth("550px");
+        dialog.setHeight("250px");
+        dialog.setCloseOnOutsideClick(false);
+
+        Button closeButton = new Button("Close", e -> {
+            dialog.close();
+            UI.getCurrent().getPage().reload();
+        });
+
+        dialogDisplay.setAlignItems(Alignment.CENTER);
+        dialogDisplay.add(new H3("Thank you for your booking!"));
+        dialogDisplay.add(new Label("Your booking request has been sent for approval."));
+        dialogDisplay.add(new Label("To check the status of your booking please use this booking id:"));
+        dialogDisplay.add(bookingId);
+        dialogDisplay.add(closeButton);
+
+        dialog.add(dialogDisplay);
     }
 
     private void saveBooking(NewBookingForm.SaveEvent event) {
         bookingRequestController.save(event.getBookingRequest());
         tableController.save(event.getTable());
-        dialog.add(new Text("Thank you for your booking!\nYour booking has been sent for approval.\nTo check the status of your booking please use this booking reference: \n" + event.getBookingRequest().getId()));
+
+        bookingId.setText(event.getBookingRequest().getId());
+        
         dialog.open();
-//        form.clearAllFields();
     }
 }
